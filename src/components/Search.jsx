9a4ee5e7cf4bf3d50e162a4e672a3ccbb8c4e1b1
suprabhosaha <suprabho04@gmail.com>
2024-08-27 01:35:11 +0530

    Search functionality implemented

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,46 @@
-const Search = () => {
-    return ( 
+import MasonryLayout from "./MasonryLayout";
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import Spinner from "./Spinner";
+import { useEffect, useState } from "react";
+
+const Search = ({ searchTerm }) => {
+
+    const [pins, setPins] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (searchTerm !== '') {
+            setLoading(true);
+
+            const query = searchQuery(searchTerm.toLowerCase());
+
+            client.fetch(query)
+                .then((data) => {
+                    setPins(data);
+                    setLoading(false);
+                })
+        }
+        else {
+            client.fetch(feedQuery)
+                .then((data) => {
+                    setPins(data);
+                    setLoading(false);
+                })
+        }
+    }, [searchTerm])
+
+    return (
         <div>
-            Search
+            {loading && <Spinner message="Searching for Images..." />}
+            {pins && pins?.length !== 0 && <MasonryLayout pins={pins} />}
+            {pins && pins?.length === 0 && searchTerm !== '' && !loading && (
+                <div className="mt-10 text-center text-xl">
+                    No Images Found!
+                </div>
+            )}
         </div>
-     );
+    );
 }
- 
+
 export default Search;
